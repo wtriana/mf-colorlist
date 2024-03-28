a287bf7b25620d1228b33139937fd710e417df05
wilmer triana WATE <winy07@gmail.com>
2024-03-27 21:18:00 -0500

    changed value argv.mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: argv.mode == 'dev' 
+    publicPath: argv.mode == 'development' 
     ? "http://localhost:3002/"
     : "https://mf-colorlist-wt.netlify.app/",
   },
