d877fab1a690e311bf15cb21efc1af0897e64079
wilmer triana WATE <winy07@gmail.com>
2024-03-27 20:30:30 -0500

    change to custom url production

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3002/",
+    publicPath: "https://mf-colorlist-wt.netlify.app/",
   },
 
   resolve: {
